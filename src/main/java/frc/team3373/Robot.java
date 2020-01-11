463c2c8e6eefc9e3574dd4329bdf971cdca1b5df
Squishy <squishycaterpillar@gmail.com>
2020-01-11 14:21:56 -0500

    added ColorSensor class

--- src/main/java/frc/team3373/Robot.java
+++ src/main/java/frc/team3373/Robot.java
@@ -7,71 +7,38 @@
 
 package frc.team3373;
 
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
 
-import com.revrobotics.ColorMatch;
-import com.revrobotics.ColorSensorV3;
-
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
-  private Joystick joy = new Joystick(0);
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
+  // Color 2
+  private ColorSensor myCs = new ColorSensor();
+
+  // Joystick
+  private SuperJoystick driver = new SuperJoystick(0);
+
   @Override
   public void robotInit() {
+
+    // Meta
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
 
   }
 
-
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
@@ -79,9 +46,7 @@ public class Robot extends TimedRobot {
     System.out.println("Auto selected: " + m_autoSelected);
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
+
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
@@ -95,41 +60,23 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
   @Override
   public void teleopPeriodic() {
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
   @Override
   public void testPeriodic() {
 
-    Color detectedColor = m_colorSensor.getColor();
-    double IR = m_colorSensor.getIR();
+    // update color sensor
+    myCs.update();
 
-    //Color COLOR_WHEEL_RED = ColorMatch.makeColor(r, g, b);
-    
+    // Show data on shuffleboard  
+    SmartDashboard.putNumber("IR", myCs.getIR());
+    SmartDashboard.putNumber("Proximity", myCs.getProximity());
+    SmartDashboard.putString("Detected Color", myCs.getWheelColor().toString());
 
-
-    SmartDashboard.putNumber("Red", detectedColor.red);
-    SmartDashboard.putNumber("Green", detectedColor.green);
-    SmartDashboard.putNumber("Blue", detectedColor.blue);
-
-
-    SmartDashboard.putNumber("IR", IR);
-    int proximity = m_colorSensor.getProximity();
-
-    SmartDashboard.putNumber("Proximity", proximity);
-
-
-
-    if (joy.getRawButtonPressed(0)) {
-      System.out.println(detectedColor.red + ", " + detectedColor.green + ", " + detectedColor.blue);
+    if (driver.isAHeld()) {
+      System.out.println(myCs.getR()+" "+myCs.getG()+" "+myCs.getB());
     }
-
   }
 }
