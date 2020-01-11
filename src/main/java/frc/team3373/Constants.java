ebf521618810d01600e7f8d4373fede7f004b496
Matthew <matthew-heller@comcast.net>
2020-01-11 12:29:20 -0500

    changed constants to finals

--- src/main/java/frc/team3373/Constants.java
+++ src/main/java/frc/team3373/Constants.java
@@ -1,19 +1,19 @@
 package frc.team3373;
 
 public class Constants{
-    public static double FLP=5;
-    public static double FLI=0.001;
-    public static double FLD=25;
+    public static final double FLP=5;
+    public static final double FLI=0.001;
+    public static final double FLD=25;
 
-    public static double FRP=5;
-    public static double FRI=0.0015;
-    public static double FRD=25;
+    public static final double FRP=5;
+    public static final double FRI=0.0015;
+    public static final double FRD=25;
+    
+    public static final double BLP=6;
+    public static final double BLI=0.0035;
+    public static final double BLD=25;
 
-    public static double BLP=6;
-    public static double BLI=0.0035;
-    public static double BLD=25;
-
-    public static double BRP=5;
-    public static double BRI=0.0015;
-    public static double BRD=15;
-}
+    public static final double BRP=5;
+    public static final double BRI=0.0015;
+    public static final double BRD=15;
+} 
